51ba4197ef7419a68614e8c370d4df73b9905d53
Dave Rench McCauley <drench56@gmail.com>
2019-09-19 12:26:18 -0400

    Update file directory structure to allow for local package installation and testing

---
+++ src/tests/test_ETL.py
@@ -0,0 +1,72 @@
+import pytest
+import pandas as pd
+import os
+
+# datatest is a pytest extension for data validation
+import datatest as dt
+
+#import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+#sys.path.insert(1, '../src/data')
+from src.data.process_data import load_data, clean_data, save_data
+
+# Feeds the DataFrame df in when pytest is run so it can be used in testing
+@pytest.fixture(scope='module')
+@dt.working_directory(__file__)
+def df():
+    return load_data('../../data/disaster_messages.csv',
+                    '../../data/disaster_categories.csv')
+
+
+@pytest.mark.mandatory
+def test_columns(df):
+    '''
+    Checks that DataFrame loading and merge was successful. 
+    Specifically checks that 5 columns are present (and no more):
+        id
+        message
+        original
+        genre
+        categories
+    '''   
+    
+    required_names = {'id', 'message', 
+                      'original', 'genre',
+                      'categories'}
+    
+    dt.validate(df.columns, required_names)
+    
+def test_all_clean_columns():
+    '''
+    Checks that the cleaned data have dropped the 'categories' column
+    and that it has 40 columns in total
+    and that 
+    '''
+    # Have to explicitly load it here since I'm using regular asserts
+    # and not datatest
+    df_temp = clean_data(load_data('../../data/disaster_messages.csv',
+                              '../../data/disaster_categories.csv'))
+    
+    assert((df_temp.columns == 'categories').sum() == 0)
+    
+    assert(len(df_temp.columns) == 40)
+    
+def test_clean_category_columns(df):
+    '''
+    Checks that column indexes 4 to 39 are all integer (0 or 1)
+    '''
+    df_temp = clean_data(df)
+    
+    for col in df.iloc[:,4:]:
+        dt.validate(df_temp[col], int)
+        
+def test_no_duplicates():
+    '''
+    Checks that no duplicate rows exist after cleaning
+    '''
+    
+    df_temp = clean_data(load_data('../../data/disaster_messages.csv',
+                              '../../data/disaster_categories.csv'))
+    
+    assert(df_temp.duplicated().sum() == 0)
+    
