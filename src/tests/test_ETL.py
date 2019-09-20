bdf1127c2eb62ec123bbc940a4474f3f5bb9da94
Dave Rench McCauley <drench56@gmail.com>
2019-09-20 15:45:19 -0400

    Update ETL tests and save conda environment

--- src/tests/test_ETL.py
+++ src/tests/test_ETL.py
@@ -12,14 +12,17 @@ from src.data_processing.process_data import load_data, clean_data, save_data
 
 # Feeds the DataFrame df in when pytest is run so it can be used in testing
 @pytest.fixture(scope='module')
-@dt.working_directory(__file__)
+#@dt.working_directory(__file__)
 def df():
-    return load_data('../../data/disaster_messages.csv',
-                    '../../data/disaster_categories.csv')
+    return load_data('data/disaster_messages.csv',
+                    'data/disaster_categories.csv')
 
+@pytest.fixture(scope='module')
+def clean_df(df):
+    return clean_data(df)
 
 @pytest.mark.mandatory
-def test_columns(df):
+def test_merged_columns(df):
     '''
     Checks that DataFrame loading and merge was successful. 
     Specifically checks that 5 columns are present (and no more):
@@ -36,51 +39,48 @@ def test_columns(df):
     
     dt.validate(df.columns, required_names)
     
-def test_all_clean_columns():
+def test_clean_no_category_columns(clean_df):
     '''
     Checks that the cleaned data have dropped the 'categories' column
     and that it has 40 columns in total
     and that 
     '''
-    # Have to explicitly load it here since I'm using regular asserts
-    # and not datatest validation
-    df_temp = clean_data(load_data('../../data/disaster_messages.csv',
-                              '../../data/disaster_categories.csv'))
     
-    assert((df_temp.columns == 'categories').sum() == 0)
     
-    assert(len(df_temp.columns) == 40)
+    assert((clean_df.columns == 'categories').sum() == 0)
     
-def test_clean_category_columns(df):
+    
+def test_clean_column_count(clean_df):
+    '''
+    Make sure there are the expected 40 columns
+    '''
+    
+    assert(len(clean_df.columns) == 40)
+    
+def test_clean_category_columns(clean_df):
     '''
     Checks that column indexes 4 to 39 are all integer (0 or 1)
     '''
-    df_temp = clean_data(df)
-    
-    for col in df.iloc[:,4:]:
-        dt.validate(df_temp[col], int)
+    for col in clean_df.iloc[:,4:]:
+        dt.validate(clean_df[col], int)
         
-def test_no_duplicates():
+def test_clean_no_duplicates(clean_df):
     '''
     Checks that no duplicate rows exist after cleaning
     '''
     
-    df_temp = clean_data(load_data('../../data/disaster_messages.csv',
-                              '../../data/disaster_categories.csv'))
-    
-    assert(df_temp.duplicated().sum() == 0)
+    assert(clean_df.duplicated().sum() == 0)
     
     
-def test_saving(tmp_path):
+def test_saving(tmp_path, clean_df):
     '''
     Make sure no errors are thrown as a result of saving the cleaned DataFrame
     to an sqlite3 database
     '''
-    df_temp = clean_data(load_data('../../data/disaster_messages.csv',
-                              '../../data/disaster_categories.csv'))
+    
     #TODO: figure out why you're getting an OperationalError 435 from sqlalchemy
         # then uncomment tests
-    #save_data(df_temp, str(tmp_path))
+    #save_data(clean_df, str(tmp_path))
     #assert(os.path.isfile(tmp_path))
     
     
